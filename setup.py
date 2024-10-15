1bba8a2fb79402ad50d5ac1f5a47e297c6ef45a6
Akash R <115867523+Akashr-18@users.noreply.github.com>
2024-10-15 19:59:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
     
 __version__ = "0.0.0"
-
 REPO_NAME = "End-to-End-Diamond-Price-Prediction"
 AUTHOR_USER_NAME = "Akashr-18"
 SRC_REPO = "DiamondRegressor"
