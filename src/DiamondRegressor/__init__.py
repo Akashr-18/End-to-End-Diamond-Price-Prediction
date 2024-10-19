b105674ac6b0b186a4eefb2b4d0d7b28766b40b5
Akash R <115867523+Akashr-18@users.noreply.github.com>
2024-10-19 18:23:13 +0530

    Update __init__.py

--- src/DiamondRegressor/__init__.py
+++ src/DiamondRegressor/__init__.py
@@ -7,7 +7,6 @@ logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 logging_dir = 'logs'
 os.makedirs(logging_dir, exist_ok=True)
 logging_filepath = os.path.join(logging_dir, "running_logs.log")
-os.makedirs(logging_dir, exist_ok=True)
 
 logging.basicConfig(
     level = logging.INFO,
